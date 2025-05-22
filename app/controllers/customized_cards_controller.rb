fbe336602f2094cfcfc2624a141c8373b60e263a
areebatahir358 <areebatahir966@gmail.com>
2025-05-22 18:10:24 +0500

    layout of customization change

--- app/controllers/customized_cards_controller.rb
+++ app/controllers/customized_cards_controller.rb
@@ -1,36 +1,32 @@
 class CustomizedCardsController < ApplicationController
   def new
     @template = Template.find(params[:template_id])
+    @customized_card = CustomizedCard.new
   end
 
   def create
-    template = Template.find(params[:template_id])
+    @template = Template.find(params[:template_id])
     name = params[:name]
     date = params[:date]
     message = params[:message]
 
     input_path = Rails.root.join("tmp", "#{SecureRandom.hex}_input.jpg")
-    File.open(input_path, "wb") { |file| file.write(template.image.download) }
+    File.open(input_path, "wb") { |file| file.write(@template.image.download) }
 
-    # Output path
     output_path = Rails.root.join("tmp", "custom_#{SecureRandom.hex}.jpg")
 
-    full_text = "#{name}\n#{date}\n#{message}"
+    ImageEditorService.add_text(
+      image_path: input_path.to_s,
+      output_path: output_path.to_s,
+      text: { name: name, date: date, message: message }
+    )
 
-   ImageEditorService.add_text(
-  image_path: input_path.to_s,
-  text: full_text,
-  output_path: output_path.to_s
-   )
-
-  
-   
-  card = CustomizedCard.create!(
-    name: name,
-    date: date,
-    message: message,
-    template: @template
-  )
+    card = CustomizedCard.create!(
+      name: name,
+      date: date,
+      message: message,
+      template: @template
+    )
     card.final_image.attach(io: File.open(output_path), filename: "custom_card.jpg")
 
     redirect_to customized_card_path(card)
@@ -39,23 +35,7 @@ class CustomizedCardsController < ApplicationController
   def show
     @card = CustomizedCard.find(params[:id])
   end
-
-  def new
-  @template = Template.find(params[:template_id])
-  @customized_card = CustomizedCard.new
-  end
 end
-
-def create
-  @template = Template.find(params[:template_id])
-  @customized_card = @template.customized_cards.build(customized_card_params)
-  if @customized_card.save
-    redirect_to @customized_card, notice: 'Card created successfully!'
-  else
-    render :new
-  end
-end
-
 
 
 
--- app/services/image_editor_service.rb
+++ app/services/image_editor_service.rb
@@ -1,14 +1,17 @@
+
 class ImageEditorService
-  def self.add_text(image_path:, text:, output_path:)
+  def self.add_text(image_path:, output_path:, text:)
     image = MiniMagick::Image.open(image_path)
 
     image.combine_options do |c|
-      c.gravity 'South'
-      c.pointsize '32'
-      c.fill 'black'
-      c.draw "text 0,10 '#{text}'"
+      c.gravity "NorthWest" # top-left corner
+      c.pointsize "24"
+      c.fill "black"
+      c.draw "text 50,100 '#{text[:name]}'"
+      c.draw "text 50,150 '#{text[:date]}'"
+      c.draw "text 50,200 '#{text[:message]}'"
     end
 
-    image.write(output_path)
+    image.write output_path
   end
 end
